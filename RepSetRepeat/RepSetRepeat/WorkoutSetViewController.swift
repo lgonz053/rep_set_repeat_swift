f4435d9d5287e1e17a6aa5ee644b9116f6347d45
Leo Gonzalez <lgonz053@gmail.com>
2019-03-22 12:05:31 -0500

    deleted some tabs. added a drop down menu for exercises

--- RepSetRepeat/RepSetRepeat/WorkoutSetViewController.swift
+++ RepSetRepeat/RepSetRepeat/WorkoutSetViewController.swift
@@ -10,9 +10,106 @@ import UIKit
 import Alamofire
 
 class WorkoutSetViewController: UIViewController {
+    var button = dropDownBtn()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        button = dropDownBtn.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        button.setTitle("Exercises", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(button)
+        
+        button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        
+        button.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 40).isActive = true
     }
 }
 
+class dropDownBtn: UIButton {
+    var dropView = dropDownView()
+    
+    var height = NSLayoutConstraint()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = UIColor.darkGray
+        
+        dropView = dropDownView.init(frame: CGRect.init(x: 0, y: 0, width: 0, height: 0))
+        
+        dropView.translatesAutoresizingMaskIntoConstraints = false
+    
+    }
+    
+    override func didMoveToSuperview() {
+        self.superview?.addSubview(dropView)
+        self.superview?.bringSubviewToFront(dropView)
+        
+        dropView.topAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        dropView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        dropView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        height = dropView.heightAnchor.constraint(equalToConstant: 0)
+    }
+    
+    var isOpen = false
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if isOpen == false {
+            
+        } else {
+            
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+class dropDownView: UIView, UITableViewDelegate, UITableViewDataSource {
+    var dropDownOptions = [String]()
+    
+    var tableView = UITableView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(tableView)
+        
+        tableView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dropDownOptions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        
+        cell.textLabel?.text = dropDownOptions[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(dropDownOptions[indexPath.row])
+    }
+}
